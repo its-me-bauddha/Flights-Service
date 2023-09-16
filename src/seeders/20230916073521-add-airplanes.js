1b9062516ffbca09c13f7b7bbb5b6510c342e8bd
its-me-bauddha <harshbauddha563@gmail.com>
2023-09-16 13:21:48 +0530

    Added seeders

---
+++ src/seeders/20230916073521-add-airplanes.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const { Op } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("Airplanes", [
+      {
+        modelNumber: "airbus450",
+        capacity: 400,
+        createdAt : new Date(),
+        updatedAt:new Date()
+      },
+
+      {
+        modelNumber: "airbus1222",
+        capacity: 594,
+        createdAt : new Date(),
+        updatedAt:new Date()
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Airplanes' ,
+    {[Op.or]:
+      [
+        {modelNumber: "airbus1222"},
+        {modelNumber: "airbus450"}
+      ]})
+  },
+};
