2755ebcf8f3932a17f0e4cbb7cde6c7739697f79
its-me-bauddha <harshbauddha563@gmail.com>
2023-09-19 14:55:03 +0530

    Added new migration that can add  a constraint in a column of Airport table .
    ```
    npx sequelize migration:generate --name update-city-aiport-association
    ```

--- src/models/airport.js
+++ src/models/airport.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.City,{
+        foreignKey:'cityId',
+        onUpdate:'CASCADE',
+        onDelete:'CASCADE'
+      })
     }
   }
   Airport.init({
