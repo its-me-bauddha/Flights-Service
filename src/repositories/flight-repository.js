2a187a473db031c0f363807457233df419e16882
its-me-bauddha <harshbauddha563@gmail.com>
2023-09-21 15:51:56 +0530

    Added queries file

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,7 +1,8 @@
 const {Sequelize} = require('sequelize');
 const CrudRepository = require('./crud-repository')
 const { Flight,Airplane,Airport ,City} = require('../models')
-const db = require('../models')
+const db = require('../models');
+const { addRowLockOnFlights } = require('./queries');
 
 class FlightRepository extends CrudRepository {
     constructor() {
@@ -49,7 +50,7 @@ class FlightRepository extends CrudRepository {
     }
 
     async updateRemainingSeats(flightId,seats,dec = true){
-        await db.sequelize.query(`SELECT * from Flights WHERE Flights.id =  ${flightId} FOR UPDATE;`)
+        await db.sequelize.query(addRowLockOnFlights(flightId));
         const flight = await Flight.findByPk(flightId);
         
         if(parseInt(dec)){
